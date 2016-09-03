436f0b3c5893a00411452689cbb65262cd3711c2
Aaron Gallagher <aaron.b.gallagher@gmail.com>
2016-09-02 18:47:35 -0700

    aopp: Use aopp kernel config Also set selinux to permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,8 @@ TARGET_OTA_ASSERT_DEVICE := htc_m8,htc_m8whl,htc_m8wl,m8,m8wl,m8wlv,m8vzw,m8whl,
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/mkbootimg.mk
-TARGET_KERNEL_CONFIG := cm_m8_defconfig
+TARGET_KERNEL_CONFIG := aopp_m8_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/m8/bluetooth
